ec9fb3ff883429a382e8e49a18d3abbf62a13c0d
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2021-01-02 21:43:24 +0500

    ci: исключение проверки pep8

--- cluedo/tests/test_main.py
+++ cluedo/tests/test_main.py
@@ -1,12 +1,14 @@
-import os, sys, inspect
+import os
+import sys
+import inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
+import main
+
 
 def test_hello():
-    import main
-
     start = {
         'version': 1,
         'session': {
