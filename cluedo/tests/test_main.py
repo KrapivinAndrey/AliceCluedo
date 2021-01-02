3c9d27da7c781ddd3874466aa9259036f0e67f44
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2021-01-02 21:34:18 +0500

    fix

--- cluedo/tests/test_main.py
+++ cluedo/tests/test_main.py
@@ -3,10 +3,10 @@ current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
-import main
-
 
 def test_hello():
+    import main
+
     start = {
         'version': 1,
         'session': {
