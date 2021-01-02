a1ae0173a4d91a18883e8a558a7fb6f65e408ce9
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2021-01-02 21:28:43 +0500

    test: Добавлен тест на приветствие

---
+++ cluedo/tests/test_main.py
@@ -0,0 +1,16 @@
+import os, sys, inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+import main
+
+
+def test_hello():
+    start = {
+        'version': 1,
+        'session': {
+            'new': True
+        }
+    }
+    assert 'Привет!' in main.handler(start, '')['response']['text']
