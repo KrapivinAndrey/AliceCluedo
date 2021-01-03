395150c5947c1e8e12350078bb63ced827a95ae7
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2021-01-03 23:23:59 +0500

    feat: Взял из AlisaBattleship

--- cluedo/alice.py
+++ cluedo/alice.py
@@ -1,3 +1,6 @@
+import json
+
+
 class AliceResponse:
 
     def __init__(self, request):
@@ -10,7 +13,30 @@ class AliceResponse:
             }
         }
 
-    def get(self, path):
+    def dumps(self):
+        return json.dumps(
+            self._response_dict,
+            ensure_ascii=False,
+            indent=2
+        )
+
+    def set_text(self, text):
+        self._response_dict['response']['text'] = text[:1024]
+        self._response_dict['response']['tts'] = text[:1024]  # по умолчанию произношение совпадает с текстом
+
+    def set_tts(self, tts):
+        self._response_dict['response']['tts'] = tts  # tts может быть длиннее за счет дополнительных звуков
+
+    def set_buttons(self, buttons):
+        self._response_dict['response']['buttons'] = buttons
+
+    def end(self):
+        self._response_dict["response"]["end_session"] = True
+
+    def __str__(self) -> str:
+        return self.dumps()
+
+    def get(self, path) :
         result = self._response_dict
         for a in path.split('.'):
             result = result[a]
